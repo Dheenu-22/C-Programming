84a8cca63dda4d86b63e17ab93dd9f19461b2da5
Dheenu-22 <dheenun2203@gmail.com>
2026-01-31 10:31:53 +0530

    Implement merge K Lists to merge k sorted lists

---
+++ merge k_sorted list.c
@@ -0,0 +1,22 @@
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+    if (l1->val < l2->val) {
+        l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
+    } else {
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
+    }
+}
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
+    if (listsSize == 0) return NULL;
+    int interval = 1;
+    while (interval < listsSize) {
+        for (int i = 0; i + interval < listsSize; i += interval * 2) {
+            lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+        }
+        interval *= 2;
+    }
+    return lists[0];
+}
